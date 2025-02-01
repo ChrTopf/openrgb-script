0302ca7972360a5869f5a355ef2b85aee3ab7a0e
ChrTopf <christoph@gabler-omb.de>
2025-02-01 14:48:39 +0100

    added flush parameter to all print calls

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import sys
 
 # initialize the interrupt signal handler
 def signal_handler(sig, frame):
-    print('This routine was interrupted by a system signal!')
+    print('This routine was interrupted by a system signal!', flush=True)
     sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
@@ -23,10 +23,10 @@ client.clear()
 # set the motherboard to the configured color
 motherboard = client.get_devices_by_type(DeviceType.MOTHERBOARD)[0]
 motherboard.set_color(primary)
-print(f"The motherboard {motherboard.name} was initialized with the primary color.")
+print(f"The motherboard {motherboard.name} was initialized with the primary color.", flush=True)
 
 mouse = client.get_devices_by_type(DeviceType.MOUSE)[0]
-print(f"The following mouse is going to be controlled: {mouse.name}")
+print(f"The following mouse is going to be controlled: {mouse.name}", flush=True)
 
 while True:
     mouse.set_mode("static")
